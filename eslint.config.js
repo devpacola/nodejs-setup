9deababc0722cdc8cfa74f30a361abcc81541206
Dalton Pacola Franco <devpacola@gmail.com>
2025-07-14 23:55:29 -0300

    Second Commit

--- eslint.config.js
+++ eslint.config.js
@@ -2,23 +2,23 @@ import eslintPluginPrettier from "eslint-plugin-prettier";
 import eslintConfigPrettier from "eslint-config-prettier";
 
 export default [
-	{
-		files: ["**/*.js"],
-		ignores: ["node_modules"],
+  {
+    files: ["**/*.js"],
+    ignores: ["node_modules"],
 
-		languageOptions: {
-			ecmaVersion: "latest",
-			sourceType: "module"
-		},
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
 
-		plugins: {
-			prettier: eslintPluginPrettier
-		},
+    plugins: {
+      prettier: eslintPluginPrettier
+    },
 
-		rules: {
-			"no-unused-vars": "warn",
-			"prettier/prettier": "warn"
-		}
-	},
-	eslintConfigPrettier
+    rules: {
+      "no-unused-vars": "warn",
+      "prettier/prettier": "warn"
+    }
+  },
+  eslintConfigPrettier
 ];
